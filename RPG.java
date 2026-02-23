d37088e6798a2288e6e2393ea11a169efba84dae
haru-java225 <h121003270225@gmail.com>
2026-02-24 00:02:51 +0900

    コメント追加

--- RPG.java
+++ RPG.java
@@ -3,14 +3,14 @@ public class RPG {
         int hit = (int) (Math.random() * 50 + 1);
         if (hit < 25) { // 出た目が25未満
             System.out.println(hit + "ダメージを与えた！");
-        } else {
+        } else { // 出た目が25以上
             System.out.println("クリティカルヒットで100ダメージを与えた！");
         }
 
         int add = (int) (Math.random() * 2 + 1);
-        if (add == 1) {
+        if (add == 1) { // 振ったサイコロの目が1のとき
             System.out.println("追撃に成功した！20ダメージを与えた！");
-        } else {
+        } else { // 振ったサイコロの目が2のとき
             System.out.println("追撃に失敗した！");
         }
     }
